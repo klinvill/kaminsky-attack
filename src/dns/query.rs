64fad930000ee02cf7743a7ea30d0f35c2e5a250
Kirby Linvill <kjlinvill@gmail.com>
2020-10-05 11:47:49 -0400

    Refactor query into a struct that can construct a corresponding message

--- src/dns/query.rs
+++ src/dns/query.rs
@@ -6,56 +6,69 @@ use crate::dns::question::Question;
 use crate::dns::types::Type;
 use rand;
 
-fn build_query(
-    hostnames: Vec<&str>,
-    qtype: Option<Type>,
-    opcode: Option<Opcode>,
-    recursion_desired: Option<bool>,
-) -> Result<QuestionMessage, String> {
-    let id = rand::random::<u16>();
+struct Query {
+    hostnames: Vec<String>,
+    qtype: Type,
+    opcode: Opcode,
+    recursion_desired: bool,
+}
 
-    if hostnames.len() > u16::max_value() as usize {
-        return Err(format!(
-            "Too many hostnames entered, cannot query for more than {} hostnames",
-            u16::max_value()
-        ));
+impl Query {
+    fn new(hostnames: Vec<String>) -> Query {
+        return Query {
+            hostnames,
+            qtype: Type::A,
+            opcode: Opcode::QUERY,
+            recursion_desired: false,
+        };
     }
-    let qdcount: u16 = hostnames.len() as u16;
 
-    let header = Header {
-        id,
-        qr: false, // query rather than response
-        opcode: opcode.unwrap_or(Opcode::QUERY),
-        aa: false,
-        tc: false, // TODO: need to ensure that the message never exceeds a length greater than that permitted by the transmission channel
-        rd: recursion_desired.unwrap_or(false),
-        ra: false,
-        z: 0,
-        rcode: 0,
-        qdcount,
-        ancount: 0,
-        nscount: 0,
-        arcount: 0,
-    };
+    fn to_message(&self) -> Result<QuestionMessage, String> {
+        if self.hostnames.len() > u16::max_value() as usize {
+            return Err(format!(
+                "Too many hostnames entered, cannot query for more than {} hostnames",
+                u16::max_value()
+            ));
+        }
 
-    let questions: Result<Vec<Question>, String> = hostnames
-        .iter()
-        .map(|hostname| {
-            let qname = Hostname::from_string(hostname)?;
-            return Ok(Question {
-                qname,
-                qtype: qtype.unwrap_or(Type::A),
-                qclass: Class::IN,
-            });
-        })
-        .collect();
+        let id = rand::random::<u16>();
 
-    let message = QuestionMessage {
-        header,
-        questions: questions?,
-    };
+        let qdcount: u16 = self.hostnames.len() as u16;
 
-    return Ok(message);
+        let header = Header {
+            id,
+            qr: false, // query rather than response
+            opcode: self.opcode,
+            aa: false,
+            tc: false, // TODO: need to ensure that the message never exceeds a length greater than that permitted by the transmission channel
+            rd: self.recursion_desired,
+            ra: false,
+            z: 0,
+            rcode: 0,
+            qdcount,
+            ancount: 0,
+            nscount: 0,
+            arcount: 0,
+        };
+
+        let questions: Result<Vec<Question>, String> = self
+            .hostnames
+            .iter()
+            .map(|hostname| {
+                let qname = Hostname::from_string(hostname)?;
+                return Ok(Question {
+                    qname,
+                    qtype: self.qtype,
+                    qclass: Class::IN,
+                });
+            })
+            .collect();
+
+        return Ok(QuestionMessage {
+            header,
+            questions: questions?,
+        });
+    }
 }
 
 #[cfg(test)]
@@ -64,14 +77,14 @@ mod tests {
     use crate::dns::header::{Header, Opcode};
     use crate::dns::hostname::Hostname;
     use crate::dns::message::QuestionMessage;
-    use crate::dns::query::build_query;
+    use crate::dns::query::Query;
     use crate::dns::question::Question;
     use crate::dns::types::Type;
 
     #[test]
-    fn simple_build_query() {
-        // TODO: How should optional function arguments be declared and supplied?
-        let message = build_query(vec!["www.example.com"], None, None, None).unwrap();
+    fn query_to_message() {
+        let query = Query::new(vec!["www.example.com".to_string()]);
+        let message = query.to_message().unwrap();
 
         let expected = QuestionMessage {
             header: Header {
