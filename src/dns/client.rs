b31ca9e706066ba9af35def842378def56b7d4de
Kirby Linvill <kjlinvill@gmail.com>
2020-10-09 12:01:44 -0400

    Add client send without recv function
    This allows a client to send a message without listening for a response

--- src/dns/client.rs
+++ src/dns/client.rs
@@ -1,6 +1,7 @@
 use crate::dns::message::Message;
 use crate::dns::query::Query;
 use std::net::UdpSocket;
+use std::time::Duration;
 
 // Max size of a DNS UDP packet as specified in IETF RFC 1035
 const DNS_MAX_UDP_SIZE: usize = 512;
@@ -10,6 +11,7 @@ pub struct Client {
     local_port: u16,
     server: String,
     port: u16,
+    timeout: Duration,
 }
 
 impl Client {
@@ -19,16 +21,54 @@ impl Client {
             local_port: 0,
             server,
             port: 53,
+            timeout: Duration::new(10, 0),
         };
     }
 
     // TODO: make sure to use an error type that encompasses the IO errors
     pub fn query(&self, request: Query) -> Result<Message, String> {
+        return self.send_message(&request.to_message()?);
+    }
+
+    pub fn send_message(&self, message: &Message) -> Result<Message, String> {
         let mut buffer = [0; DNS_MAX_UDP_SIZE];
+        let socket = self.connect()?;
+
+        let message_payload = message.to_bytes();
+        match socket.send(message_payload.as_slice()) {
+            Err(e) => return Err(e.to_string()),
+            _ => (),
+        };
+
+        let size = match socket.recv(&mut buffer) {
+            Err(e) => return Err(e.to_string()),
+            Ok(sz) => sz,
+        };
+
+        return Message::parse(&buffer[..size]);
+    }
+
+    pub fn send_message_no_recv(&self, message: &Message) -> Result<(), String> {
+        let socket = self.connect()?;
+
+        let message_payload = message.to_bytes();
+        match socket.send(message_payload.as_slice()) {
+            Err(e) => return Err(e.to_string()),
+            _ => (),
+        };
+
+        return Ok(());
+    }
+
+    pub fn connect(&self) -> Result<UdpSocket, String> {
         let local_address = format!("{}:{}", self.local_host, self.local_port);
         let socket = match UdpSocket::bind(local_address) {
             Err(e) => return Err(e.to_string()),
             Ok(sock) => sock,
+        };
+        match socket.set_write_timeout(Some(self.timeout)) {
+            Err(e) => return Err(e.to_string()),
+            _ => (),
         };
 
         println!("Bound to local address {}", socket.local_addr().unwrap());
@@ -44,17 +84,6 @@ impl Client {
             socket.peer_addr().unwrap()
         );
 
-        let request_payload = request.to_message()?.to_bytes();
-        match socket.send(request_payload.as_slice()) {
-            Err(e) => return Err(e.to_string()),
-            _ => (),
-        };
-
-        let size = match socket.recv(&mut buffer) {
-            Err(e) => return Err(e.to_string()),
-            Ok(sz) => sz,
-        };
-
-        return Message::parse(&buffer[..size]);
+        return Ok(socket);
     }
 }
